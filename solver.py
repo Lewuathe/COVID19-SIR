80f58b663869a56a61a42060e7b3f0980eb7bf90
Lewuathe <lewuathe@me.com>
2020-03-11 17:41:16 +0900

    New data

--- solver.py
+++ solver.py
@@ -12,14 +12,52 @@ R_0 = 0
 START_DATE = {
   'Japan': '1/22/20',
   'Italy': '1/31/20',
-  'South Korea': '1/22/20',
-  'Iran': '2/19/20'
+  'Republic of Korea': '1/22/20',
+  'Iran (Islamic Republic of)': '2/19/20'
 }
 
-def load_confirmed(country):
-  df = pd.read_csv('data/time_series_19-covid-Confirmed.csv')
-  country_df = df[df['Country/Region'] == country]
-  return country_df.iloc[0].loc[START_DATE[country]:]
+class Learner(object):
+    def __init__(self, country, loss):
+        self.country = country
+        self.loss = loss
+
+    def load_confirmed(self, country):
+      df = pd.read_csv('data/time_series_19-covid-Confirmed.csv')
+      country_df = df[df['Country/Region'] == country]
+      return country_df.iloc[0].loc[START_DATE[country]:]
+
+    def extend_index(self, index, new_size):
+        values = index.values
+        current = datetime.strptime(index[-1], '%m/%d/%y')
+        while len(values) < new_size:
+            current = current + timedelta(days=1)
+            values = np.append(values, datetime.strftime(current, '%m/%d/%y'))
+        return values
+
+    def predict(self, beta, gamma, data, country):
+        predict_range = 150
+        new_index = self.extend_index(data.index, predict_range)
+        size = len(new_index)
+        def SIR(t, y):
+            S = y[0]
+            I = y[1]
+            R = y[2]
+            return [-beta*S*I, beta*S*I-gamma*I, gamma*I]
+        extended_actual = np.concatenate((data.values, [None] * (size - len(data.values))))
+        return new_index, extended_actual, solve_ivp(SIR, [0, size], [S_0,I_0,R_0], t_eval=np.arange(0, size, 1))
+
+    def train(self):
+        data = self.load_confirmed(self.country)
+        optimal = minimize(loss, [0.001, 0.001], args=(data), method='L-BFGS-B', bounds=[(0.0, 0.1), (0.0, 0.1)])
+        print(optimal)
+        beta, gamma = optimal.x
+        print(f"R_0={beta*100/gamma}%")
+        new_index, extended_actual, prediction = self.predict(beta, gamma, data, self.country)
+        df = pd.DataFrame({'Actual': extended_actual, 'S': prediction.y[0], 'I': prediction.y[1], 'R': prediction.y[2]}, index=new_index)
+        fig, ax = plt.subplots(figsize=(15, 10))
+        ax.set_title(self.country)
+        df.plot(ax=ax)
+        fig.savefig(f"{self.country}.png")
 
 def loss(point, data):
     size = len(data)
@@ -34,42 +72,14 @@ def loss(point, data):
     print(f"point={point}, loss={l}")
     return l
 
-def extend_index(index, new_size):
-    values = index.values
-    current = datetime.strptime(index[-1], '%m/%d/%y')
-    while len(values) < new_size:
-        current = current + timedelta(days=1)
-        values = np.append(values, datetime.strftime(current, '%m/%d/%y'))
-    return values
+# learner = Learner('Japan', loss)
+# learner.train()
 
-def plot(beta, gamma, data, country):
-    predict_range = 150
-    new_index = extend_index(data.index, predict_range)
-    size = len(new_index)
-    def SIR(t, y):
-        S = y[0]
-        I = y[1]
-        R = y[2]
-        return [-beta*S*I, beta*S*I-gamma*I, gamma*I]
-    solution = solve_ivp(SIR, [0, size], [S_0,I_0,R_0], t_eval=np.arange(0, size, 1))
+learner = Learner('Italy', loss)
+learner.train()
 
-    extended_actual = np.concatenate((data.values, [None] * (size - len(data.values))))
-    df = pd.DataFrame({'Actual': extended_actual, 'S': solution.y[0], 'I': solution.y[1], 'R': solution.y[2]}, index=new_index)
-    fig, ax = plt.subplots(figsize=(15, 10))
-    ax.set_title(country)
-    df.plot(ax=ax)
-    fig.savefig(f"{country}.png")
+# learner = Learner('Republic of Korea', loss)
+# learner.train()
 
-def predict(country):
-    data = load_confirmed(country)
-    optimal = minimize(loss, [0.001, 0.001], args=(data), method='L-BFGS-B', bounds=[(0.0, 0.1), (0.0, 0.1)])
-    print(optimal)
-    beta, gamma = optimal.x
-    print(f"R_0={beta*100/gamma}%")
-    plot(beta, gamma, data, country)
-
-
-# predict('Japan')
-# predict('South Korea')
-# predict('Italy')
-predict('Iran')
+# learner = Learner('Iran (Islamic Republic of)', loss)
+# learner.train()
